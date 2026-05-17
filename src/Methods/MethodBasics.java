87a0a87cb1cd5a3667a329a27e678a8e70b13d87
Hirdyansh Varshney <hirdyanshvarshney22@gmail.com>
2026-05-17 18:53:29 +0530

    added MethodBasics - first method program

---
+++ src/Methods/MethodBasics.java
@@ -0,0 +1,10 @@
+package Methods;
+
+public class MethodBasics {
+    public static void main(String[] args) {
+        greet();
+    }
+    static void greet() {
+        System.out.println("Hello world!, Good Morning");
+    }
+}
